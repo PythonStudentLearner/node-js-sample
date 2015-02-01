9a50ad87c4c79b9243e612d8eefd073035ee1ebb
PythonStudentLearner <PythondtudentLearner@outlook.com>
2015-02-01 17:11:25 +0000

    simple text change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Git - aws -heroku')
+  response.send('------------------------------------------------------------------Git - aws -heroku')
 })
 
 app.listen(app.get('port'), function() {
