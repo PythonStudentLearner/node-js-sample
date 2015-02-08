103bc2151e243a482aef5f10cdaab7d8d411df53
PythonStudentLearner <PythondtudentLearner@outlook.com>
2015-02-08 15:11:39 +0000

    updates 8th feb

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(' Promotion !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-})
+  response.send('8th Feb Updates ') })
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
