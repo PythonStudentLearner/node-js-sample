dddd02e92bdd19b24e0489142327a97a582bdc9f
PythonStudentLearner <PythondtudentLearner@outlook.com>
2015-02-08 18:55:39 +0000

    current

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 var express = require('express')
 var app = express();
 
-app.set('port', (process.env.PORT || 8080))
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync('./bootstrap/index.html');
 
-app.use(express.static(__dirname + '/public'))
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end(index);
+}).listen(9615);
 
-app.get('/', function(request, response) {
-  response.send('bootstrap/index.html') })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//app.set('port', (process.env.PORT || 8080))
+
+//app.use(express.static(__dirname + '/public'))
+
+//app.get('/', function(request, response) {
+//  response.send('bootstrap/index.html') })
+
+//app.listen(app.get('port'), function() {
+// console.log("Node app is running at localhost:" + app.get('port'))
+//})
